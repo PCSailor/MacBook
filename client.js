e38d72110b81bef44864e905252b264be4b77294
Phil <PC@Stephanies-MacBook-Pro.local>
2017-02-07 08:00:44 -0600

    Initial Commit

---
+++ client.js
@@ -0,0 +1,116 @@
+console.log(new Date().getFullYear() + " Javascript Code test"); // Display current year is JS
+//document.write(scriptTestLine.getFullYear());
+var scriptTestLine = new Date(); console.log(scriptTestLine); // Displays complete date is JS
+// document.write(new Date().getFullYear()); // Displays current year on HTML page
+console.log("--------------------------------------------->");
+console.log("Function Syntax \n function NAME (Parameters are the names, Arguments are the numbers) {\n Inside the braces is the process portion, do some code \n return result from the process}");
+console.log("While Loop Statement:  while(*some expression is true*) {*do this code!*}// While Loop Example");
+console.log("For Loop:  for ( Variable ;  Condition;  Increment) \n {Code execution}");
+console.log("For Loop:  for ( *start with this-initialize variable counter* ;  *loop if this expression is true/condition to see if loop will continue*;  *do this after each loop*) \n {*in each loop, do this code!*}");
+console.log("for (var i = 1; i <= somelist.length; i++) {}  // blank for loop");
+console.log("Read.Write.Speak");
+console.log("CMD-delete deletes entire line");
+console.log("--------------------------------------------->");
+//---------------------------------------------------------------------------------------------------------------------------------------------------->
+// console.log("Classroom Day one_2017-02-06");
+// // 5 basic types:
+// var someNum = 7 * 3;
+// var someBoolean = true;
+// var someString = "This was what Luke was hoping for";
+// var someArray = ["one", "two", 3, "four"];
+// var someObject = {
+//   cohort: "Phi",
+//   nickname: "Phi Fters",
+//   studentCount: 16
+// };
+// console.log(someNum);
+// console.log(someBoolean);
+// console.log(someString);
+// console.log(someArray);
+// console.log("This is great");
+// console.log(someObject);
+
+// var x = 3;
+// var y = 6;
+// var sum = x + y;
+// sum = sum / 3 + 2 + sum +x;
+// console.log(sum);
+// console.log();
+
+//console.log(one(sum)); // returns 5
+// Type Coersion
+// var firstWord = "Hello";
+// var secondWord = "World";
+// var thridWord = 1;
+// var firstThing = 'tucket';
+// var secondThing = "7"
+// // NaN is a typeOf number
+// console.log(firstWord + " " + secondWord);
+
+// Conditionals
+// = is assigning
+// == is equals without the typeOf
+// === is equals with the typeOf
+// var thing = 5;
+// if(thing >= 5.5 && thing <=5|| thing == 3) {
+//   console.log("taco");
+//   } else if (thing >= 5) {
+//     console.log("maybe a taco");
+//   }else {
+//     console.log("not a taco");
+//   }
+// Loops
+// While Loop // Used for a logicical condition
+// var bobLost = true;
+// var counter = 0;
+// while(bobLost) {
+//   counter++;
+//   if(counter > 1) {
+//     bobLost = false;
+//   }
+//   console.log("We are searching for Bob");
+// }
+// For Loop // Used to do a loop determined number of times
+// for(var i = 0; i < 100; i++) {
+//   console.log("This is the " + (i + 1) + "th time through.");
+// }
+
+// Function
+// 1st way to write same function, order/placement doesn't matter
+// sayHelloToSomeone("my little friend");
+// function sayHelloToSomeone(name) {
+//   var greeting = "Hello " + name;
+//   console.log(greeting);
+// }
+// // 2nd way to write function but the order of code matters
+// var sayHelloToSomeone = function (name) {
+//   var greeting = "Hello " + name;
+//   console.log(greeting);
+// }
+// sayHelloToSomeone("my little friend");
+
+
+console.log(sayHelloToTwoPeople("Bob", "Abby"));
+
+function sayHelloToTwoPeople(nameOne, nameTwo) {
+var greeting = ("Hello " + nameOne + " and " + nameTwo + "!");
+return greeting;
+}
+// Consoles "Hello Bob and Abby!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//aedfdfvsaffabw
